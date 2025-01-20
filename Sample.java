82ae0fa7e956f933681f09de8e05d1d4bcf86933
BIKRAM-ADAK <44100077+BIKRAM-ADAK@users.noreply.github.com>
2025-01-20 18:52:43 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,3 @@
-int number = 42;
-double pi = 3.14159;
-String name = "John Doe";
 
-System.out.println("The number is: " + number);
-System.out.println("The value of pi is: " + pi);
-System.out.println("My name is: " + name);
+
+System.out.println("My name is: " );
