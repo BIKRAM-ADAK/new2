f91d8f52876308339e47fd18e02db8e62c01cf5e
BIKRAM-ADAK <44100077+BIKRAM-ADAK@users.noreply.github.com>
2025-01-20 18:51:10 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!"); 
-    }
-}
+int number = 42;
+double pi = 3.14159;
+String name = "John Doe";
+
+System.out.println("The number is: " + number);
+System.out.println("The value of pi is: " + pi);
+System.out.println("My name is: " + name);
